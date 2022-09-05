4492c15f44015e9c7a45052935eb977a428c19b4
chagu <chaguer4@gmail.com>
2022-09-05 22:37:30 +0100

    menuResto

---
+++ MenuResto/Form1.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace EX3PETP8
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ck1 = new System.Windows.Forms.CheckedListBox();
+            this.ck2 = new System.Windows.Forms.CheckedListBox();
+            this.ck3 = new System.Windows.Forms.CheckedListBox();
+            this.ck4 = new System.Windows.Forms.CheckedListBox();
+            this.listbox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ck1
+            // 
+            this.ck1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ck1.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ck1.FormattingEnabled = true;
+            this.ck1.Items.AddRange(new object[] {
+            "Salade de Chef",
+            "Salade Nécoise",
+            "Salade de Pattes",
+            "Salade Variée",
+            "Salade Marocaine ",
+            "Salade Verte",
+            "Salade De Saison"});
+            this.ck1.Location = new System.Drawing.Point(16, 118);
+            this.ck1.Margin = new System.Windows.Forms.Padding(4);
+            this.ck1.Name = "ck1";
+            this.ck1.Size = new System.Drawing.Size(217, 165);
+            this.ck1.TabIndex = 0;
+            // 
+            // ck2
+            // 
+            this.ck2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ck2.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ck2.FormattingEnabled = true;
+            this.ck2.Items.AddRange(new object[] {
+            "Brochette De boeuf",
+            "Brochette De Viande Hachée",
+            "Brochette De Foie",
+            "Brochette De poulet"});
+            this.ck2.Location = new System.Drawing.Point(243, 118);
+            this.ck2.Margin = new System.Windows.Forms.Padding(4);
+            this.ck2.Name = "ck2";
+            this.ck2.Size = new System.Drawing.Size(219, 165);
+            this.ck2.TabIndex = 1;
+            // 
+            // ck3
+            // 
+            this.ck3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ck3.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ck3.FormattingEnabled = true;
+            this.ck3.Items.AddRange(new object[] {
+            "Créme Caramel",
+            "Mouse au Chocolat",
+            "Glasse",
+            "Tarte Au Pomme",
+            "Salade De fruit "});
+            this.ck3.Location = new System.Drawing.Point(16, 334);
+            this.ck3.Margin = new System.Windows.Forms.Padding(4);
+            this.ck3.Name = "ck3";
+            this.ck3.Size = new System.Drawing.Size(217, 119);
+            this.ck3.TabIndex = 2;
+            this.ck3.SelectedIndexChanged += new System.EventHandler(this.checkedListBox3_SelectedIndexChanged);
+            // 
+            // ck4
+            // 
+            this.ck4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ck4.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ck4.FormattingEnabled = true;
+            this.ck4.Items.AddRange(new object[] {
+            "Jus D\'orange",
+            "Jus De Pomme",
+            "Jus de Citron",
+            "Boissons Gazeaux "});
+            this.ck4.Location = new System.Drawing.Point(243, 334);
+            this.ck4.Margin = new System.Windows.Forms.Padding(4);
+            this.ck4.Name = "ck4";
+            this.ck4.Size = new System.Drawing.Size(219, 119);
+            this.ck4.TabIndex = 3;
+            // 
+            // listbox
+            // 
+            this.listbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.listbox.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listbox.FormattingEnabled = true;
+            this.listbox.ItemHeight = 21;
+            this.listbox.Location = new System.Drawing.Point(479, 126);
+            this.listbox.Margin = new System.Windows.Forms.Padding(4);
+            this.listbox.Name = "listbox";
+            this.listbox.Size = new System.Drawing.Size(204, 214);
+            this.listbox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.label1.Location = new System.Drawing.Point(87, 80);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(81, 35);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Salade";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Italic);
+            this.label2.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.label2.Location = new System.Drawing.Point(268, 80);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 35);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Plat Principal";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Italic);
+            this.label3.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.label3.Location = new System.Drawing.Point(61, 295);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 35);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Desserts";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Italic);
+            this.label4.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.label4.Location = new System.Drawing.Point(284, 295);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 35);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Boissons";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Italic);
+            this.label5.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.label5.Location = new System.Drawing.Point(476, 87);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(200, 35);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Votre Commandes";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(143, 465);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 41);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Commander";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.DragEnter += new System.Windows.Forms.DragEventHandler(this.button2_DragEnter);
+            this.button1.DragLeave += new System.EventHandler(this.button2_DragLeave);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(300, 465);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 41);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Initialiser";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.DragEnter += new System.Windows.Forms.DragEventHandler(this.button2_DragEnter);
+            this.button2.DragLeave += new System.EventHandler(this.button2_DragLeave);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::EX3PETP8.Properties.Resources.moroccan_tajine_isolated_vector_8281567;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(88, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(479, 383);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(205, 123);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mode de Paiement";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(120, 87);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(77, 28);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Payer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(4, 76);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(90, 21);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Visa Card";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(4, 48);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(76, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Espéce";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(4, 20);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(78, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Chéque";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label6.Location = new System.Drawing.Point(190, 15);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label6.Size = new System.Drawing.Size(94, 47);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Menu";
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(701, 521);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listbox);
+            this.Controls.Add(this.ck4);
+            this.Controls.Add(this.ck3);
+            this.Controls.Add(this.ck2);
+            this.Controls.Add(this.ck1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox ck1;
+        private System.Windows.Forms.CheckedListBox ck2;
+        private System.Windows.Forms.CheckedListBox ck3;
+        private System.Windows.Forms.CheckedListBox ck4;
+        private System.Windows.Forms.ListBox listbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ MenuResto/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EX3PETP8
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void checkedListBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Width = 374;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Voulez vous vraiment Confirmer Votre Commande ?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+                listbox.Items.Clear();
+            for (int i = 0; i < ck1.CheckedItems.Count; i++)
+                listbox.Items.Add(ck1.CheckedItems[i].ToString());
+            for (int i = 0; i < ck2.CheckedItems.Count; i++)
+                listbox.Items.Add(ck2.CheckedItems[i].ToString());
+            for (int i = 0; i < ck3.CheckedItems.Count; i++)
+                listbox.Items.Add(ck3.CheckedItems[i].ToString());
+            for (int i = 0; i < ck4.CheckedItems.Count; i++)
+                listbox.Items.Add(ck4.CheckedItems[i].ToString());
+            this.Width = 540;
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Width = 374;
+            for (int i = 0; i < ck1.Items.Count; i++)
+                ck1.SetItemChecked(i, false);
+            for (int i = 0; i < ck2.Items.Count; i++)
+                ck2.SetItemChecked(i, false);
+            for (int i = 0; i < ck3.Items.Count; i++)
+                ck3.SetItemChecked(i, false);
+           // for (int i = 0; i < ck3.Items.Count; i++)
+            //    ck4.SetItemChecked(i, false);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+                MessageBox.Show(radioButton1.Text, "Mode de paiment");
+            if (radioButton2.Checked == true)
+                MessageBox.Show(radioButton1.Text, "Mode de paiment");
+            if (radioButton3.Checked == true)
+                MessageBox.Show(radioButton1.Text, "Mode de paiment");
+        }
+
+        private void button2_DragEnter(object sender, DragEventArgs e)
+        {
+            button1.BackColor = Color.Red;
+        }
+
+        private void button2_DragLeave(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.White;
+        }
+    }
+}
